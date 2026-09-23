bf22218373bfd983a4b0d5d47d818a6bde5e6eb5
sharadhi-hr <sharadhihrsharadhi@gmail.com>
2026-09-23 11:13:38 +0530

    Demo code modified

--- Demo.java
+++ Demo.java
@@ -1,5 +1,6 @@
 class Demo{
 	public static void main(String[]args) {
-		System.out.println("Hello git");
+		System.out.println("Hello world");
+		System.out.println("Welcome to GitHub!!!...");
 	}
 }
